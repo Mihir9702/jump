7ceca97aa3a6a49a704855e508126a6fa045308b
Mihir9702 <mkpatel9702@gmail.com>
2023-10-07 10:56:20 -0400

    remove audio

--- js/background.js
+++ js/background.js
@@ -1,17 +1,23 @@
 export default class Background {
-    constructor(x, y, context, ch) {
-        this.pos = { x, y }
-        this.background = new Image();
-        this.background.src = "./assets/backgrounds.jpg";
-        this.context = context;
-        this.ch = ch;
-        this.backgroundAudio = new Audio();
-        this.backgroundAudio.src = "./assets/springField96.wav";
-        this.backgroundAudio.volume = 0.2;
-    }
+  constructor(x, y, context, ch) {
+    this.pos = { x, y };
+    this.background = new Image();
+    this.background.src = "./assets/backgrounds.jpg";
+    this.context = context;
+    this.ch = ch;
+    // this.backgroundAudio = new Audio();
+    // this.backgroundAudio.src = "./assets/springField96.wav";
+    // this.backgroundAudio.volume = 0.2;
+  }
 
-    draw() {
-        this.backgroundAudio.play();
-        this.context.drawImage(this.background, this.pos.x, this.pos.y, this.background.width * 2, this.ch);
-    }
+  draw() {
+    // this.backgroundAudio.play();
+    this.context.drawImage(
+      this.background,
+      this.pos.x,
+      this.pos.y,
+      this.background.width * 2,
+      this.ch
+    );
+  }
 }
